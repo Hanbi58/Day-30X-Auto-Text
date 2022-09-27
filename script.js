7854ec03d94b809ffa4eb7613be626bec9082ff2
Hanbi58 <hanxiaonan2012@gmail.com>
2022-09-26 20:34:06 -0700

    My version Done

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-alert('1')
+const text = document.querySelector(".text");
+
+const msg = "I love Xiaomaomi, so much, can someone stop me?";
+
+const msgArr = msg.split("");
+console.log(msgArr);
+text.textContent = "";
+msgArr.forEach((letter, idx) => {
+  setTimeout(() => {
+    text.textContent += letter;
+  }, idx * 100);
+});
